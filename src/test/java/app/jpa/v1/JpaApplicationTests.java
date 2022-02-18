81700a9213e94674dcc7036af216d2c1cd042808
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-02-18 14:19:01 +0300

    Jpa with spring

---
+++ src/test/java/app/jpa/v1/JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package app.jpa.v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
